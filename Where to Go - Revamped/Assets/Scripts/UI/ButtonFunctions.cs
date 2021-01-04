e24e21f72a782a7ff216dffe6ade821bb88e37ca
Alex Markoe <acmark527@gmail.com>
2021-01-04 16:39:39 -0500

    Branch for UI testing, created main menu scene with functional buttons

---
+++ Where to Go - Revamped/Assets/Scripts/UI/ButtonFunctions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonFunctions : MonoBehaviour
+{
+    //Load a scene
+    //by name when the button is clicked
+    public void LoadOnClick(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    //Exit the application
+    public void ExitOnClick()
+    {
+        Application.Quit();
+    }
+}
