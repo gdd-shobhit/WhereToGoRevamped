eaade573ae6fb5da89d29c5ef0a94340df129f16
Alex Markoe <acmark527@gmail.com>
2021-01-19 10:58:38 -0500

    Initialization

---
+++ Where to Go - Revamped/Assets/Scripts/UI/GrappleIndicator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrappleIndicator : MonoBehaviour
+{
+    float targetY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetY = transform.parent.position.y + 1.2f;
+        StartCoroutine("IndicatorAnim");
+    }
+
+    IEnumerator IndicatorAnim()
+    {
+        while(transform.position.y < targetY - 0.2f)
+        {
+            targetY = transform.parent.position.y + 1.2f;
+            transform.position = new Vector3(transform.position.x, Mathf.Lerp(transform.position.y, targetY, 0.01f), 1);
+            yield return null;
+        }
+    }
+}
